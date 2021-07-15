1a4606b4209afc326da3513746f423a7bc8e8609
mliu59 <61032765+mliu59@users.noreply.github.com>
2021-07-14 20:53:10 -0500

    landing page changes

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 function generateHeader(navSize = "20px", titleSize = "45px", width = "55%", margin = "auto", title = "MILES LIU") {
     var header = document.getElementById("sitenav");
-    header.innerHTML = `<div class="sitenavbar" style="width: ` + width + `; margin: ` + margin + `;">
+    header.innerHTML = `<div class="sitenavbar" id="navblock" style="width: ` + width + `; margin: ` + margin + `;">
     <div id="pagetitle" style="font-size: ` + titleSize + `;">` + title + `</div>
     <div class="nav">
         <a class="nav-link navtext" style="font-size: ` + navSize + `;" href="https://mliu59.github.io/">HOME</a>
